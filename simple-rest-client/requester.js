2aacbf11947c32a477299378c3c6b4e374f56827
Stefan Auditor <stefan.auditor@erdfisch.de>
2010-12-28 14:03:25 +0100

    adding readable status codes as per rfc2616

--- simple-rest-client/requester.js
+++ simple-rest-client/requester.js
@@ -1,3 +1,52 @@
+// Status codes as per rfc2616
+// @see http://tools.ietf.org/html/rfc2616#section-10
+var statusCodes = new Array();
+// Informational 1xx
+statusCodes[100] = 'Continue';
+statusCodes[101] = 'Switching Protocols';
+// Successful 2xx
+statusCodes[200] = 'OK';
+statusCodes[201] = 'Created';
+statusCodes[202] = 'Accepted';
+statusCodes[203] = 'Non-Authoritative Information';
+statusCodes[204] = 'No Content';
+statusCodes[205] = 'Reset Content';
+statusCodes[206] = 'Partial Content';
+// Redirection 3xx
+statusCodes[300] = 'Multiple Choices';
+statusCodes[301] = 'Moved Permanently';
+statusCodes[302] = 'Found';
+statusCodes[303] = 'See Other';
+statusCodes[304] = 'Not Modified';
+statusCodes[305] = 'Use Proxy';
+statusCodes[307] = 'Temporary Redirect';
+// Client Error 4xx
+statusCodes[400] = 'Bad Request';
+statusCodes[401] = 'Unauthorized';
+statusCodes[402] = 'Payment Required';
+statusCodes[403] = 'Forbidden';
+statusCodes[404] = 'Not Found';
+statusCodes[405] = 'Method Not Allowed';
+statusCodes[406] = 'Not Acceptable';
+statusCodes[407] = 'Proxy Authentication Required';
+statusCodes[408] = 'Request Time-out';
+statusCodes[409] = 'Conflict';
+statusCodes[410] = 'Gone';
+statusCodes[411] = 'Length Required';
+statusCodes[412] = 'Precondition Failed';
+statusCodes[413] = 'Request Entity Too Large';
+statusCodes[414] = 'Request-URI Too Long';
+statusCodes[415] = 'Unsupported Media Type';
+statusCodes[416] = 'Requested range not satisfiable';
+statusCodes[417] = 'Expectation Failed';
+// Server Error 5xx
+statusCodes[500] = 'Internal Server Error';
+statusCodes[501] = 'Not Implemented';
+statusCodes[502] = 'Bad Gateway';
+statusCodes[503] = 'Service Unavailable';
+statusCodes[504] = 'Gateway Time-out';
+statusCodes[505] = 'HTTP Version not supported';
+
 function grow(id) {
   var textarea = document.getElementById(id);
   var newHeight = textarea.scrollHeight;
@@ -73,7 +122,7 @@ function readResponse() {
       if(this.status == 0) {
         throw('Status = 0');
       }
-      $("#responseStatus").html(this.status);
+      $("#responseStatus").html(this.status+' '+statusCodes[this.status]);
       $("#responseHeaders").val(jQuery.trim(this.getAllResponseHeaders()));
       $("#codeData").html(jQuery.trim(this.responseText).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
 
