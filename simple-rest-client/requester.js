10a8d10343ef5977078be3e81f3dfc9266aefbe0
unknown <Abhinav@.(none)>
2011-06-26 00:37:52 +0530

    Working history feature

--- simple-rest-client/requester.js
+++ simple-rest-client/requester.js
@@ -47,158 +47,248 @@ statusCodes[503] = 'Service Unavailable';
 statusCodes[504] = 'Gateway Time-out';
 statusCodes[505] = 'HTTP Version not supported';
 
+var keyItemCount = "imRestClient.itemCount";
+var keyItemBase = "imRestClient.item";
+var keyRequests = "imRestClient.requests";
+var requests;
+
 function grow(id) {
-  var textarea = document.getElementById(id);
-  var newHeight = textarea.scrollHeight;
-  var currentHeight = textarea.clientHeight;
-  if (newHeight == 0 || $("#"+id).val() == "") {
-    newHeight = 20;
-  }
-  textarea.style.height = newHeight + 'px';
+    var textarea = document.getElementById(id);
+    var newHeight = textarea.scrollHeight;
+    var currentHeight = textarea.clientHeight;
+    if (newHeight == 0 || $("#" + id).val() == "") {
+        newHeight = 20;
+    }
+    textarea.style.height = newHeight + 'px';
 }
 
 function clearFields() {
-  $("#response").css("display", "");
-  $("#loader").css("display", "");
-  $("#responsePrint").css("display", "none");
+    $("#response").css("display", "");
+    $("#loader").css("display", "");
+    $("#responsePrint").css("display", "none");
 
-  $("#responseStatus").html("");
-  $("#responseHeaders").val("");
-  $("#codeData").text("");
+    $("#responseStatus").html("");
+    $("#responseHeaders").val("");
+    $("#codeData").text("");
 
-  $("#responseHeaders").height(20);
-  $("#headers").height(20);
-  $("#postputdata").height(20);
+    $("#responseHeaders").height(20);
+    $("#headers").height(20);
+    $("#postputdata").height(20);
 
-  $("#respHeaders").css("display", "none");
-  $("#respData").css("display", "none");
+    $("#respHeaders").css("display", "none");
+    $("#respData").css("display", "none");
 }
 
 function sendRequest() {
-  clearFields();
-  if($("#url").val() != "") {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = readResponse;
-    try {
-      xhr.open($("input[type=radio]:checked").val(), $("#url").val(), true);
-      var headers = $("#headers").val();
-      headers = headers.split("\n");
-      for (var i = 0; i < headers.length; i++) {
-        var header = headers[i].split(": ");
-        if (header[1])
-            xhr.setRequestHeader(header[0],header[1]);
-      }
-      if(jQuery.inArray($("input[type=radio]:checked").val(), ["post", "put"]) > -1) {
-        xhr.send($("#postputdata").val());
-      } else {
-        xhr.send("");
-      }
-    }
-    catch(e){
-      console.log(e);
-      $("#responseStatus").html("<span style=\"color:#FF0000\">"+chrome.i18n.getMessage("bad_request")+"</span>");
-      $("#respHeaders").css("display", "none");
-      $("#respData").css("display", "none");
+    clearFields();
+    if ($("#url").val() != "") {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = readResponse;
+        try {
+            xhr.open($("input[type=radio]:checked").val(), $("#url").val(), true);
+            var headers = $("#headers").val();
 
-      $("#loader").css("display", "none");
-      $("#responsePrint").css("display", "");
-    }
-  } else {
-    console.log("no uri");
-    $("#responseStatus").html("<span style=\"color:#FF0000\">"+chrome.i18n.getMessage("bad_request")+"</span>");
-    $("#respHeaders").css("display", "none");
-    $("#respData").css("display", "none");
+            var url = $("#url").val();
+            var method = $("input[type=radio]:checked").val();
 
-    $("#loader").css("display", "none");
-    $("#responsePrint").css("display", "");
-  }
+            saveRequest(url, method, headers);
+
+            headers = headers.split("\n");
+            for (var i = 0; i < headers.length; i++) {
+                var header = headers[i].split(": ");
+                if (header[1])
+                    xhr.setRequestHeader(header[0], header[1]);
+            }
+            if (jQuery.inArray($("input[type=radio]:checked").val(), ["post", "put"]) > -1) {
+                xhr.send($("#postputdata").val());
+            } else {
+                xhr.send("");
+            }
+        }
+        catch(e) {
+            console.log(e);
+            $("#responseStatus").html("<span style=\"color:#FF0000\">" + chrome.i18n.getMessage("bad_request") + "</span>");
+            $("#respHeaders").css("display", "none");
+            $("#respData").css("display", "none");
+
+            $("#loader").css("display", "none");
+            $("#responsePrint").css("display", "");
+        }
+    } else {
+        console.log("no uri");
+        $("#responseStatus").html("<span style=\"color:#FF0000\">" + chrome.i18n.getMessage("bad_request") + "</span>");
+        $("#respHeaders").css("display", "none");
+        $("#respData").css("display", "none");
+
+        $("#loader").css("display", "none");
+        $("#responsePrint").css("display", "");
+    }
 }
 
 function readResponse() {
-  grow('headers');
-  grow('postputdata');
-  if (this.readyState == 4) {
-    try {
-      if(this.status == 0) {
-        throw('Status = 0');
-      }
-      $("#responseStatus").html(this.status+' '+statusCodes[this.status]);
-      $("#responseHeaders").val(jQuery.trim(this.getAllResponseHeaders()));
-      var debugurl = /X-Debug-URL: (.*)/i.exec($("#responseHeaders").val());
-      if (debugurl) {
-	  $("#debugLink").attr('href', debugurl[1]).html(debugurl[1]);
-	  $("#debugLinks").css("display", "");
-      }
-      $("#codeData").html(jQuery.trim(this.responseText).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+    grow('headers');
+    grow('postputdata');
+    if (this.readyState == 4) {
+        try {
+            if (this.status == 0) {
+                throw('Status = 0');
+            }
+            $("#responseStatus").html(this.status + ' ' + statusCodes[this.status]);
+            $("#responseHeaders").val(jQuery.trim(this.getAllResponseHeaders()));
+            var debugurl = /X-Debug-URL: (.*)/i.exec($("#responseHeaders").val());
+            if (debugurl) {
+                $("#debugLink").attr('href', debugurl[1]).html(debugurl[1]);
+                $("#debugLinks").css("display", "");
+            }
+            $("#codeData").html(jQuery.trim(this.responseText).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
 
-      $("#respHeaders").css("display", "");
-      $("#respData").css("display", "");
+            $("#respHeaders").css("display", "");
+            $("#respData").css("display", "");
 
-      $("#loader").css("display", "none");
-      $("#responsePrint").css("display", "");
+            $("#loader").css("display", "none");
+            $("#responsePrint").css("display", "");
 
-      grow('responseHeaders');
+            grow('responseHeaders');
 
-      $.chili.options.automatic.active = false;
-      $.chili.options.decoration.lineNumbers = false;
-      var $chili = $('#codeData').chili();
+            $.chili.options.automatic.active = false;
+            $.chili.options.decoration.lineNumbers = false;
+            var $chili = $('#codeData').chili();
+        }
+        catch(e) {
+            $("#responseStatus").html("No response.");
+            $("#respHeaders").css("display", "none");
+            $("#respData").css("display", "none");
+
+            $("#loader").css("display", "none");
+            $("#responsePrint").css("display", "");
+        }
     }
-    catch(e) {
-      $("#responseStatus").html("No response.");
-      $("#respHeaders").css("display", "none");
-      $("#respData").css("display", "none");
-
-      $("#loader").css("display", "none");
-      $("#responsePrint").css("display", "");
-    }
-  }
 }
 
 function toggleData() {
-  if(jQuery.inArray($("input[type=radio]:checked").val(), ["post", "put"]) > -1) {
-    $("#data").css("display", "");
-  } else {
-    $("#data").css("display", "none");
-  }
+    if (jQuery.inArray($("input[type=radio]:checked").val(), ["post", "put"]) > -1) {
+        $("#data").css("display", "");
+    } else {
+        $("#data").css("display", "none");
+    }
 }
 
 function init() {
-  $("#url").width($("#purl").width()-80-30);
-  $("#headers").width($("#pheaders").width()-80-30);
-  $("#postputdata").width($("#data").width()-80-30);
+    $("#url").width($("#purl").width() - 80 - 30);
+    $("#headers").width($("#pheaders").width() - 80 - 30);
+    $("#postputdata").width($("#data").width() - 80 - 30);
 
-  $("#responseHeaders").width($("#respHeaders").width()-80-30);
-  $("#responseData").width($("#respHeaders").width()-80-30);
+    $("#responseHeaders").width($("#respHeaders").width() - 80 - 30);
+    $("#responseData").width($("#respHeaders").width() - 80 - 30);
 
-  $("#response").css("display", "none");
-  $("#loader").css("display", "");
-  $("#responsePrint").css("display", "none");
-  $("#sep").css("display", "none");
+    $("#response").css("display", "none");
+    $("#loader").css("display", "");
+    $("#responsePrint").css("display", "none");
+    $("#sep").css("display", "none");
 
-  $("#data").css("display", "none");
+    $("#data").css("display", "none");
 
-  $("#responseStatus").html("");
-  $("#respHeaders").css("display", "none");
-  $("#respData").css("display", "none");
+    $("#responseStatus").html("");
+    $("#respHeaders").css("display", "none");
+    $("#respData").css("display", "none");
 
-  $("#submit").click(function() { sendRequest(); return false; });
-  $("#reset").click(function() { location.reload(); });
-  $(".radio").change(function() { toggleData(); });
-  $(".radio").focus(function() { toggleData(); });
+    $("#submit").click(function() {
+        sendRequest();
+        return false;
+    });
+    $("#reset").click(function() {
+        location.reload();
+    });
+    $(".radio").change(function() {
+        toggleData();
+    });
+    $(".radio").focus(function() {
+        toggleData();
+    });
+
+    //Initialize the localStarage requsts array if not present
+    if(!localStorage[keyRequests]) {
+        var r = [];
+        localStorage[keyRequests] = JSON.stringify(r);
+    }
+
+}
+
+//History management functions
+function saveRequest(url, method, headers) {
+    var id = guid();
+    var requestItem = {
+        "id": id,
+        "url": url,
+        "method": method,
+        "headers": headers
+    };
+
+    requests[requests.length] = requestItem;
+    localStorage[keyRequests] = JSON.stringify(requests);
+    addRequestToHistory(url, id);
+}
+
+function addRequestToHistory(url, id) {
+    var itemString = "<li><a href=\"javascript:void(0);\"";
+    itemString += " onclick=\"loadRequest('" + id + "')\" ";
+    itemString += "class=\"itemLink\" id=\"item-" + id + "\">" + url + "</a></li>";
+    $("#historyItems").prepend(itemString);
+}
+
+function getAllSavedRequests() {
+    var url;
+    var itemString;
+    
+    requests = JSON.parse(localStorage[keyRequests]);
+    var itemCount = requests.length;
+
+    for(var i = itemCount - 1; i >= 0; i--) {
+        url = requests[i].url;
+        id = requests[i].id;
+        itemString = "<li><a href=\"javascript:void(0);\"";
+        itemString += " onclick=\"loadRequest('" + id + "')\" ";
+        itemString += "class=\"itemLink\" id=\"item-" + id + "\">" + url + "</a></li>";
+        $("#historyItems").append(itemString);
+    }
+}
+
+function loadRequest(id) {
+    var itemCount = requests.length;
+    for(var i = itemCount - 1; i >= 0; i--) {
+        if(requests[i].id === id) {
+            break;
+        }
+    }
+
+    $('#url').val(requests[i].url);
+//    $('input[name="' + requests[i].method + '"]').attr('checked', true);
+    $('input[name="method"]').attr("checked", false);
+    $('input[id="' + requests[i].method + '"]').attr("checked", true);
+    $('#headers').val(requests[i].headers);
 }
 
 function lang() {
-  $('._msg_').each(function () {
-    var val = $(this).html();
-    $(this).html(chrome.i18n.getMessage(val));
-  });
-  $('._msg_val_').each(function () {
-    var val = $(this).val();
-    $(this).val(chrome.i18n.getMessage(val));
-  });
+    $('._msg_').each(function () {
+        var val = $(this).html();
+        $(this).html(chrome.i18n.getMessage(val));
+    });
+    $('._msg_val_').each(function () {
+        var val = $(this).val();
+        $(this).val(chrome.i18n.getMessage(val));
+    });
+}
+
+function S4() {
+   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+}
+function guid() {
+   return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
 
 $(document).ready(function() {
-  lang();
-  init();
+    lang();
+    init();
+    getAllSavedRequests();
 });
+
