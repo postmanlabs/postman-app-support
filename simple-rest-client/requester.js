a4796473b7ffcf33be8516103c93eeaeb28259a6
Abhinav Asthana <rickyast@gmail.com>
2011-12-31 14:29:08 +0530

    Added help for HTTP Status Codes from Wikipedia

--- simple-rest-client/requester.js
+++ simple-rest-client/requester.js
@@ -16,54 +16,6 @@
  specific language governing permissions and limitations
  under the License.
  */
-// Status codes as per rfc2616
-// @see http://tools.ietf.org/html/rfc2616#section-10
-var statusCodes = new Array();
-// Informational 1xx
-statusCodes[100] = 'Continue';
-statusCodes[101] = 'Switching Protocols';
-// Successful 2xx
-statusCodes[200] = 'OK';
-statusCodes[201] = 'Created';
-statusCodes[202] = 'Accepted';
-statusCodes[203] = 'Non-Authoritative Information';
-statusCodes[204] = 'No Content';
-statusCodes[205] = 'Reset Content';
-statusCodes[206] = 'Partial Content';
-// Redirection 3xx
-statusCodes[300] = 'Multiple Choices';
-statusCodes[301] = 'Moved Permanently';
-statusCodes[302] = 'Found';
-statusCodes[303] = 'See Other';
-statusCodes[304] = 'Not Modified';
-statusCodes[305] = 'Use Proxy';
-statusCodes[307] = 'Temporary Redirect';
-// Client Error 4xx
-statusCodes[400] = 'Bad Request';
-statusCodes[401] = 'Unauthorized';
-statusCodes[402] = 'Payment Required';
-statusCodes[403] = 'Forbidden';
-statusCodes[404] = 'Not Found';
-statusCodes[405] = 'Method Not Allowed';
-statusCodes[406] = 'Not Acceptable';
-statusCodes[407] = 'Proxy Authentication Required';
-statusCodes[408] = 'Request Time-out';
-statusCodes[409] = 'Conflict';
-statusCodes[410] = 'Gone';
-statusCodes[411] = 'Length Required';
-statusCodes[412] = 'Precondition Failed';
-statusCodes[413] = 'Request Entity Too Large';
-statusCodes[414] = 'Request-URI Too Long';
-statusCodes[415] = 'Unsupported Media Type';
-statusCodes[416] = 'Requested range not satisfiable';
-statusCodes[417] = 'Expectation Failed';
-// Server Error 5xx
-statusCodes[500] = 'Internal Server Error';
-statusCodes[501] = 'Not Implemented';
-statusCodes[502] = 'Bad Gateway';
-statusCodes[503] = 'Service Unavailable';
-statusCodes[504] = 'Gateway Time-out';
-statusCodes[505] = 'HTTP Version not supported';
 
 var presetDetails = {
     "oauth-request":[
@@ -424,7 +376,16 @@ function readResponse() {
 
                 $('#modalResponseError').modal('show');
             }
-            $("#responseStatus").html(this.status + ' ' + statusCodes[this.status]);
+            var responseCode = {
+                'code': this.status,
+                'name': httpStatusCodes[this.status]['name'],
+                'detail': httpStatusCodes[this.status]['detail']
+            };
+
+            $('#pstatus').html('');
+            $('#itemResponseCode').tmpl([responseCode]).appendTo('#pstatus');
+            $('.responseCode').popover();
+
             setResponseHeaders(this.getAllResponseHeaders());
 
             var debugurl = /X-Debug-URL: (.*)/i.exec($("#responseHeaders").val());
