3f52cda669b77d3c7ae92364f9b6e2bdba0baafc
Abhinav Asthana <rickyast@gmail.com>
2012-09-09 00:26:24 +0530

    Added more collection selenium tests

--- tests/selenium/selenium_test.py
+++ tests/selenium/selenium_test.py
@@ -301,15 +301,69 @@ class PostmanTestsCollections(PostmanTests):
         self.test_delete_collection()
         self.test_download_collection()
         self.test_import_collection_from_url()
+        self.browser.quit()
 
     def test_switch_to_collections(self):
-        pass
+        collection_tab = self.browser.find_element_by_css_selector("#sidebar-selectors li:nth-of-type(2)")
+        collection_tab.click()
+
+        sidebar_section_collections = self.browser.find_element_by_id("sidebar-section-collections")
+        if sidebar_section_collections.get_attribute("style").find("block") > 0:
+            self.print_success("test_switch_to_collections")
+        else:
+            self.print_failed("test_switch_to_collections")
 
     def test_create_collection_with_modal(self):
-        pass
+        add_link = self.browser.find_element_by_css_selector("#collections-options a:nth-of-type(1)")
+        add_link.click()
+        time.sleep(0.5)
+
+        new_collection_blank = self.browser.find_element_by_id("new-collection-blank")
+        new_collection_blank.clear()
+        new_collection_blank.send_keys("Test collection")
+
+        new_collection_submit = self.browser.find_element_by_css_selector("#modal-new-collection .modal-footer .btn-primary")
+        new_collection_submit.click()
+
+        time.sleep(0.25)
+
+        collection_items = self.browser.find_elements_by_css_selector("#collection-items li")
+
+        if len(collection_items) > 0:
+            self.print_success("test_create_collection_with_modal")
+        else:
+            self.print_failed("test_create_collection_with_modal")
 
     def test_create_collection_with_request(self):
-        pass
+        self.set_url_field(self.browser, "http://httpbin.org/delete")
+        method_select = self.browser.find_element_by_id("request-method-selector")    
+        Select(method_select).select_by_value("DELETE")
+        send_button = self.browser.find_element_by_id("submit-request")
+        send_button.click()
+
+        # Waits for the response
+        self.get_codemirror_value(self.browser)
+
+        add_to_collection = self.browser.find_element_by_id("add-to-collection")
+        add_to_collection.click()
+
+        time.sleep(0.5)
+
+        new_collection_input = self.browser.find_element_by_id("new-collection")
+        new_collection_input.clear()
+        new_collection_input.send_keys("Existing request collection")
+        
+        submit_button = self.browser.find_element_by_css_selector("#modal-add-to-collection .modal-footer .btn-primary")
+        submit_button.click()
+        
+        time.sleep(0.5)
+
+        collection_items = self.browser.find_elements_by_css_selector("#collection-items li")
+        if len(collection_items) > 1:
+            self.print_success("test_create_collection_with_request")
+        else:
+            self.print_failed("test_create_collection_with_request")
+        
 
     def test_add_request_to_existing_collection(self):
         pass
@@ -333,7 +387,8 @@ class PostmanTestsCollections(PostmanTests):
 def main():
     # PostmanTestsRequests().run()
     # PostmanTestsHistory().run()
-    PostmanTestsLayout().run()
+    # PostmanTestsLayout().run()
+    PostmanTestsCollections().run()
 
 if __name__ == "__main__":
     main()
