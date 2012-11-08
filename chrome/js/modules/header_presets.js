41d6238aff7aa926b1361f513abe958dbb64517a
Abhinav Asthana <rickyast@gmail.com>
2012-11-09 00:00:52 +0530

    Adding data in indexedDB

--- chrome/js/modules/header_presets.js
+++ chrome/js/modules/header_presets.js
@@ -1,21 +1,69 @@
 pm.headerPresets = {
-    presets: [],
+    presets:[],
 
-    init: function() {
+    init:function () {
         pm.headerPresets.loadPresets();
-        $('#header-presets-keyvaleditor').keyvalueeditor('init');
-        $('#headers-keyvaleditor-actions-manage-presets').on("click", function() {
+
+        var params = {
+            placeHolderKey:"Key",
+            placeHolderValue:"Value",
+            deleteButton:'<img class="deleteButton" src="img/delete.png">'
+        };
+
+        $("#header-presets-keyvaleditor").keyvalueeditor("init", params);
+        $("#headers-keyvaleditor-actions-manage-presets").on("click", function () {
             pm.headerPresets.showManager();
+        });
+
+        $(".header-presets-actions-add").on("click", function () {
+            pm.headerPresets.showEditor();
+        });
+        
+        $(".header-presets-actions-back").on("click", function () {
+            pm.headerPresets.showList();
+        });
+        
+        $(".header-presets-actions-submit").on("click", function() {
+            pm.headerPresets.addHeaderPreset();
         });
     },
 
-    loadPresets: function() {
-        pm.indexedDB.headerPresets.getAllHeaderPresets(function(items) {
+    loadPresets:function () {
+        pm.indexedDB.headerPresets.getAllHeaderPresets(function (items) {
             pm.headerPresets.presets = items;
         });
     },
 
-    showManager: function() {
-        $('#modal-header-presets').modal("show");
+    showManager:function () {
+        $("#modal-header-presets").modal("show");
+    },
+
+    showList:function () {
+        $("#header-presets-list-wrapper").css("display", "block");
+        $("#header-presets-editor").css("display", "none");
+    },
+
+    showEditor:function () {
+        $("#modal-header-presets .modal-footer").css("display", "block");
+        $("#header-presets-list-wrapper").css("display", "none");
+        $("#header-presets-editor").css("display", "block");
+    },
+
+    addHeaderPreset: function() {
+        var name = $("#header-presets-editor-name").val();
+        var headers = $("#header-presets-keyvaleditor").keyvalueeditor("getValues");
+        var id = guid();
+
+        var headerPreset = {
+            "id": id,
+            "name": name,
+            "headers": headers,
+            "timestamp":new Date().getTime()
+        };
+
+        pm.indexedDB.headerPresets.addHeaderPreset(headerPreset, function() {
+            console.log("Added succesfully");
+        });
+
     }
 };
