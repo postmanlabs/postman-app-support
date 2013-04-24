a26aca55e128cfc058b1a2d21fcbf08ccd289275
Abhinav Asthana <rickyast@gmail.com>
2013-04-24 22:26:26 +0530

    Added dummy JSON file for testing

--- tests/server/json_without_header.php
+++ tests/server/json_without_header.php
@@ -1,3 +1,28 @@
 <?php
-
-phpinfo();
+?>
+{
+    "data": "",
+    "json": null,
+    "origin": "122.167.83.127",
+    "form": {
+        "foo": "value_foo",
+        "bar": "value_bar"
+    },
+    "args": {},
+    "url": "http://httpbin.org/put",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31",
+        "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundaryLSqLzUItnL87GaF9",
+        "Connection": "close",
+        "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+        "Origin": "chrome-extension://ljkndjhokjnonidfaggiacifldihhjmg",
+        "Accept": "*/*",
+        "Content-Length": "242",
+        "Host": "httpbin.org",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Cache-Control": "no-cache",
+        "Accept-Encoding": "gzip,deflate,sdch",
+        "Cookie": "_gauges_unique_year=1; _gauges_unique=1; _gauges_unique_month=1"
+    },
+    "files": {}
+}
