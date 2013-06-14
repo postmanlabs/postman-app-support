24d053b79d2935a715a85388989cbaaf5b61a67d
Abhinav Asthana <rickyast@gmail.com>
2013-06-14 22:40:19 +0530

    Updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,7 @@
 /*global module:false*/
 module.exports = function(grunt) {
   // Project configuration.
-  grunt.initConfig({
-    meta: {
-      version: '0.1.0',
-      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '* http://PROJECT_WEBSITE/\n' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-        'YOUR_NAME; Licensed MIT */'
-    },
+  grunt.initConfig({    
     concat: {
       dist: {
         src: ['chrome/js/modules/*.js'],
