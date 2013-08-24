b28f54f10e52f0edd558cd8da802bbcb929a8548
Abhinav Asthana <rickyast@gmail.com>
2013-08-24 19:27:46 +0530

    Added tooltips

--- chrome/html/requester/templates.js
+++ chrome/html/requester/templates.js
@@ -740,7 +740,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (stack1 = helpers.id) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.id; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
-    + "\">";
+    + "\"\n		data-original-title=\"Click to add to request\"\n		data-placement=\"top\"\n		rel=\"tooltip\" >";
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
