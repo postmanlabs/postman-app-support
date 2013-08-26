828fc83ed408a31cf36667e6607e72f58dae63b4
Abhinav Asthana <rickyast@gmail.com>
2013-08-26 18:21:30 +0530

    Reverted web url

--- chrome/js/requester/base.js
+++ chrome/js/requester/base.js
@@ -38,8 +38,7 @@ if (pm.isTesting) {
 }
 else {
     pm.databaseName = "postman";
-    // pm.webUrl = "https://www.getpostman.com";
-    pm.webUrl = "http://localhost/postman/html";
+    pm.webUrl = "https://www.getpostman.com";
 }
 
 
