f67443f4b9298dd195c43fc0fa2bee6de92d3f26
Abhijit Kane <abhijitkane@gmail.com>
2014-10-20 20:10:09 +0530

    Fixing iframe height

--- chrome/js/modules/request.js
+++ chrome/js/modules/request.js
@@ -899,7 +899,7 @@ pm.request = {
                 var cleanResponseText = pm.request.response.stripScriptTag(pm.request.response.text);
                 pm.filesystem.renderResponsePreview("response.html", cleanResponseText, "html", function (response_url) {
                     $("#response-as-preview").html("<iframe></iframe>");
-                    $("#response-as-preview iframe").attr("src", response_url);
+                    $("#response-as-preview>iframe").attr("src", response_url).attr("height",800);
                 });
             }
 
@@ -1078,7 +1078,7 @@ pm.request = {
                         var cleanResponseText = pm.request.response.stripScriptTag(pm.request.response.text);
                         pm.filesystem.renderResponsePreview("response.html", cleanResponseText, "html", function (response_url) {
                             $("#response-as-preview").html("<iframe></iframe>");
-                            $("#response-as-preview iframe").attr("src", response_url);
+	                        $("#response-as-preview>iframe").attr("src", response_url).attr("height",800);
                         });
                     }
                 }
