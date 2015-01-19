c2f248222a6a364893febdb965516b1b2c721c57
Abhijit Kane <abhijitkane@gmail.com>
2015-01-19 13:20:52 +0530

    Adding datalogics logo

--- chrome/js/modules/layout.js
+++ chrome/js/modules/layout.js
@@ -76,13 +76,19 @@ pm.layout = {
         supportContent += "<div class='supporter-image supporter-image-mashape'>";
         supportContent += "<a href='http://www.getpostman.com/r?url=https://www.mashape.com/?utm_source=chrome%26utm_medium=app%26utm_campaign=postman' target='_blank'>";
         supportContent += "<img src='img/supporters/mashape-new.png'/></a></div>";
-        supportContent += "<div class='supporter-tag'>Consume or provide cloud services with the Mashape API Platform.</div></div>";
+        supportContent += "<div class='supporter-tag'>Consume or provide cloud services with the Mashape API Platform</div></div>";
 
         supportContent += "<div class='supporter clearfix'>";        
         supportContent += "<div class='supporter-image supporter-image-mashape'>";
         supportContent += "<a href='http://www.getpostman.com/r?url=http://restlet.com/?utm_source=POSTMAN' target='_blank'>";
         supportContent += "<img src='img/supporters/restlet-new.png'/></a></div>";
-        supportContent += "<div class='supporter-tag'>The all-in-one platform for web APIs.</div></div>";
+        supportContent += "<div class='supporter-tag'>The all-in-one platform for web APIs</div></div>";
+
+        supportContent += "<div class='supporter clearfix'>";        
+        supportContent += "<div class='supporter-image supporter-image-datalogics'>";
+        supportContent += "<a href='http://www.getpostman.com/r?url=http://www.datalogics.com//?utm_source=POSTMAN' target='_blank'>";
+        supportContent += "<img src='img/supporters/datalogics.jpg'/></a></div>";
+        supportContent += "<div class='supporter-tag'>Adobe eBook and PDF technologies for developers</div></div>";
 
 
         var donateTimeout;
